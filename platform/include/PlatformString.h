38afa078edf86654f0503820b8d5ffa73ff5821f
hogsy <markelswo@gmail.com>
2015-10-26 16:10:43 +0000

    Added missing header.

---
+++ platform/include/PlatformString.h
@@ -0,0 +1,33 @@
+/*	Copyright (C) 2011-2015 OldTimes Software
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#ifndef PLATFORMSTRING_H
+#define	PLATFORMSTRING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	char *p_strcpy(char *destination, const char *source);
+	char *p_strncpy(char *destination, const char *source, size_t num);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // PLATFORMSTRING_H
