cdc689f3fe3119d019bc609dd0ea1dbebbe08c7c
hogsy <markelswo@gmail.com>
2016-01-07 02:12:43 +0000

    Added heart-rate icon for HUD.

--- menu/menu_hud.c
+++ menu/menu_hud.c
@@ -25,6 +25,14 @@ ConsoleVariable_t	cv_menuhudcrosshair = { "menu_hud_crosshair", "1", true, false
 
 Material_t *hud_mnumbers, *hud_micons, *hud_mcrosshairs;
 
+// MUST match with entries in hud\icons.material!
+enum
+{
+	HUD_ICON_HEALTH,
+	HUD_ICON_ARMOR,
+	HUD_ICON_HEARTRATE,
+};
+
 void HUD_Initialize()
 {
 	hud_mnumbers = Engine.LoadMaterial("menu/hud/num");
@@ -44,16 +52,15 @@ void HUD_DrawNumber(int x, int y, int w, int h, int value)
 	Engine.DrawMaterialSurface(hud_mnumbers, value, x, y, w, h, 1);
 }
 
-void HUD_DrawNumbers(int x, int y, int value)
+void HUD_DrawNumbers(int x, int y, int value, bool offset)
 {
-	// 999
 	if (value >= 100)
 		HUD_DrawNumber(x - 64, y, 32, 32, (value / 100));
-	else x -= 32;
+	else if (offset) x -= 32;
 
 	if (value >= 10)
 		HUD_DrawNumber(x - 32, y, 32, 32, (value % 100));
-	else x -= 32;
+	else if (offset) x -= 32;
 
 	HUD_DrawNumber(x, y, 32, 32, (value % 10));
 }
@@ -88,11 +95,13 @@ void HUD_Draw(void)
 	if (armor < 0)
 		armor = 0;
 
+	Engine.DrawMaterialSurface(hud_micons, HUD_ICON_HEARTRATE, 70, g_menuheight - 70, 64, 64, 1.0f);
+
 	// Health
-	Engine.DrawMaterialSurface(hud_micons, 0, 70, g_menuheight - 70, 32, 32, 1.0f);
-	HUD_DrawNumbers(166, g_menuheight - 70, health);
+	Engine.DrawMaterialSurface(hud_micons, HUD_ICON_HEALTH, 70, g_menuheight - 70, 32, 32, 1.0f);
+	HUD_DrawNumbers(170, g_menuheight - 70, health, true);
 
 	// Armor
-	Engine.DrawMaterialSurface(hud_micons, 1, 70, g_menuheight - 110, 32, 32, 1.0f);
-	HUD_DrawNumbers(166, g_menuheight - 110, armor);
+	Engine.DrawMaterialSurface(hud_micons, HUD_ICON_ARMOR, 70, g_menuheight - 110, 32, 32, 1.0f);
+	HUD_DrawNumbers(170, g_menuheight - 110, armor, true);
 }
